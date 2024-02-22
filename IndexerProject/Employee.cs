b5b442782dc45d02cbbd0f4e1a02c34d06d0ce83
waqar <wakku9780@gmail.com>
2024-02-23 00:30:47 +0530

    Add project files.

---
+++ IndexerProject/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerProject
+{
+   public class Employee
+    {
+        int Eno;
+        double salary;
+        string Ename, Job, Dname, Location;
+
+        public Employee(int  Eno,string Ename,string Job,double Salary,string Dname,string Location)
+        {
+            this.Eno = Eno;
+            this.Ename = Ename;
+            this.Job = Job;
+            this.salary = Salary;
+            this.Dname = Dname;
+            this.Location = Location;
+
+        }
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0) return Eno;
+                else if (index == 1) return Ename;
+                else if (index == 2) return Job;
+                else if (index == 3) return salary;
+                else if (index == 4) return Dname;
+                else  if(index==5)   return Location;
+                return null;
+                
+
+
+            }
+        }
+
+    }
+}
---
+++ IndexerProject/TestEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerProject
+{
+    public class TestEmployee
+    {
+        static void Main()
+        {
+            Employee Emp = new Employee(1001, "Scott", "Manager", 10000.00, "IT", "Hyderabad");
+
+            Console.WriteLine("Eno: " + Emp[0]);
+            Console.WriteLine("Ename: " + Emp[1]);
+            Console.WriteLine("Job: " + Emp[2]);
+            Console.WriteLine("Salary: " + Emp[3]);
+            Console.WriteLine("Dname: " + Emp[4]);
+            Console.WriteLine("Location: " + Emp[5]);
+
+            Console.ReadLine();
+        }
+    }
+}
